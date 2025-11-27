a4544d9179540ebd394cc99ba9b04df8542d30a5
Felix Shepherd <felixshepherd470@gmail.com>
2025-11-27 22:27:47 +0100

    price update

--- treeCutter.js
+++ treeCutter.js
@@ -2,13 +2,15 @@
 
 let currentTrees = 0;
 let totalTrees = 0;
-let clickPower = 100;
-let clickMultiplier = 100;
+let clickPower = 1;
+let clickMultiplier = 1;
 
-const upgrade1Cost = 100;
-const upgrade2Cost = 500;
-const upgrade3Cost = 2000;
-const upgrade4Cost = 10000;
+const upgradePriceMultiplyer = 1.1
+
+let upgrade1Cost = 25;
+let upgrade2Cost = 500;
+let upgrade3Cost = 2000;
+let upgrade4Cost = 10000;
 
 let upgrade1ammount = 0;
 let upgrade2ammount = 0;
@@ -20,10 +22,10 @@ let upgrade2Power = 50;
 let upgrade3Power = 200;
 let upgrade4Power = 1000;
 
+let cost1text, cost2text, cost3text, cost4text;
 let upgradeContainer1, upgradeContainer2, upgradeContainer3, upgradeContainer4;
 
 window.onload = function() {
-    const upgradeArea = document.getElementById("upgrades");
     window.counterDisplay = document.getElementById("counter");
     window.counterDisplay.innerText = `Trees Cut: ${currentTrees}`;
 
@@ -31,6 +33,16 @@ window.onload = function() {
     upgradeContainer2 = document.getElementById("container2");
     upgradeContainer3 = document.getElementById("container3");
     upgradeContainer4 = document.getElementById("container4");
+
+    cost1text = document.getElementById("cost1")
+    cost2text = document.getElementById("cost2")
+    cost3text = document.getElementById("cost3")
+    cost4text = document.getElementById("cost4") 
+
+    cost1text.innerText = `Cost: ${upgrade1Cost}`;
+    cost2text.innerText = `Cost: ${upgrade2Cost}`;
+    cost3text.innerText = `Cost: ${upgrade3Cost}`;
+    cost4text.innerText = `Cost: ${upgrade4Cost}`;
 
     setInterval(upgrade1Tick, 1000);
     setInterval(upgrade2Tick, 1000);
@@ -50,6 +62,9 @@ function buyUpgrade1() {
         spawnImage("assets/lumber.jpg", upgradeContainer1)
         window.counterDisplay.innerText = `Trees Cut: ${currentTrees}`;
         upgrade1ammount += 1;
+        upgrade1Cost *= upgradePriceMultiplyer
+        Math.round(upgrade1Cost, 0)
+        cost1text.innerText = upgrade1Cost
     }
 }
 function buyUpgrade2() {
@@ -58,6 +73,9 @@ function buyUpgrade2() {
         spawnImage("assets/chainsaw-man-the-movie-reze-arc-character-visuals-v0-66mz75zt7eqe1.webp", upgradeContainer2)
         window.counterDisplay.innerText = `Trees Cut: ${currentTrees}`;
         upgrade2ammount += 1;
+        upgrade2Cost *= upgradePriceMultiplyer
+        Math.round(upgrade2Cost, 0)
+        cost2text.innerText = upgrade2Cost
     }
 }
 function buyUpgrade3() {
@@ -66,6 +84,9 @@ function buyUpgrade3() {
         spawnImage("assets/treechomper.jpg", upgradeContainer3)
         window.counterDisplay.innerText = `Trees Cut: ${currentTrees}`;
         upgrade3ammount += 1
+        upgrade3Cost *= upgradePriceMultiplyer
+        Math.round(upgrade3Cost, 0)
+        cost3text.innerText = upgrade3Cost
     }
 }
 function buyUpgrade4() {
@@ -74,6 +95,9 @@ function buyUpgrade4() {
         spawnImage("assets/treecutternator.jpg", upgradeContainer4)
         window.counterDisplay.innerText = `Trees Cut: ${currentTrees}`;
         upgrade4ammount += 1;
+        upgrade4Cost *= upgradePriceMultiplyer
+        Math.round(upgrade4Cost, 0)
+        cost4text.innerText = upgrade4Cost
     }
 }
 
@@ -84,7 +108,6 @@ function spawnImage(image, container){
     Image.width = 50
     Image.alt = "Upgrade Image";
     container.appendChild(Image);
-
 }
 
 function upgrade1Tick(){
